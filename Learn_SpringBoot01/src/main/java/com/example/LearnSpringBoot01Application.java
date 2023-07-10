9ab641da2a78bc7ef0d25793f6ac4add2ef99fee
Dog2077 <1002302241@qq.com>
2023-07-10 16:41:36 +0800

    Add Learn_SpringBoot01

---
+++ Learn_SpringBoot01/src/main/java/com/example/LearnSpringBoot01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBoot01Application.class, args);
+    }
+
+}
