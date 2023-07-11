3fdd889d9c059fa0bf387718a90a37069725ddc0
Dog2077 <1002302241@qq.com>
2023-07-11 08:35:15 +0800

    整合web框架

--- Learn_SpringBoot02/src/main/java/com/example/controller/MainController.java
+++ Learn_SpringBoot02/src/main/java/com/example/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.entity.Vtuber;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,13 @@ public class MainController {
     @ResponseBody
     public String index () {
         return "Fxxk SpringBoot";
+    }
+
+    @RequestMapping("/vtuber")
+    @ResponseBody
+    public Vtuber vtuber () {
+        Vtuber vtuber = new Vtuber("Hiiro", "Cat", 14);
+        return vtuber;
     }
 
 }
---
+++ Learn_SpringBoot02/src/main/java/com/example/entity/Vtuber.java
@@ -0,0 +1,12 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Vtuber {
+    String name;
+    String sex;
+    Integer age;
+}
