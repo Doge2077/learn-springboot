452f5fdf5d1f85ca7946ca0ef5e90256e8aef997
Dog2077 <1002302241@qq.com>
2023-07-13 20:32:40 +0800

    Add SpringDemo

---
+++ SpringDemo/demo-backend/src/test/java/com/example/demobackend/DemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
