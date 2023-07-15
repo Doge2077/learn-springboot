becdfcf8d57e9c981aa08d93f96c99cf5b0859a4
Dog2077 <1002302241@qq.com>
2023-07-16 01:15:28 +0800

    Add register page

--- SpringDemo/demo-frontend/src/App.vue
+++ SpringDemo/demo-frontend/src/App.vue
@@ -6,6 +6,17 @@
     <router-view/>
 </template>
 
-<style scoped>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.no-select {
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
 
 </style>
