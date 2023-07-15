159faf50fa28bef69bf791041de2da6fdbb3c1c7
Dog2077 <1002302241@qq.com>
2023-07-15 09:14:19 +0800

    Add login page

--- SpringDemo/demo-frontend/src/main.js
+++ SpringDemo/demo-frontend/src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
